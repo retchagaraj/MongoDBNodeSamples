dd154d8b05bc9107556466aa5cc22ff6744d3b65
retchagaraj <retchagaraj.inforios@gmail.com>
2022-02-17 12:28:02 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// app.get('/api/goals', (req,res) => {
+//     res.status(200).json({message:'Get Goals'})
+// })
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server Started On ${port}`))
